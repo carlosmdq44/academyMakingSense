8a2b00315e8cc3b4f1c0e764a2b2888009eba091
carlosmdq44 <carlos_mdq44@hotmail.com>
2021-08-19 12:41:44 -0300

    Primer ejercicio realizado en el curso de udemy

---
+++ Excercises1Udemy/Excercises1Udemy/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Excercises1Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// EJERCICIO NUMERO 1 
+            /* int numero=0;
+
+            Console.WriteLine("Ingrese un numero");
+
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero>=0 && numero<=10)
+            {
+                Console.WriteLine("valor ingresado al sistema es VALIDO");
+            }
+            else
+            {
+                Console.WriteLine("Valor ingresado al sistema es  NO VALIDO");
+            } */
+
+            //// EJERCICIO NUMERO 2
+            /* int a = 0;
+             int b = 0;
+
+             Console.WriteLine("Ingrese primer numero ");
+             a = int.Parse(Console.ReadLine());
+
+             Console.WriteLine("Ingrese segundo numero ");
+             b = int.Parse(Console.ReadLine());
+
+             if (a > b) {
+                 Console.WriteLine("El primer numero ingresado es el maximo ");
+             }else {
+                 Console.WriteLine("El segundo numero ingresado es el maximo ");
+             }
+            */
+
+            //// EJERCICIO NUMERO 3
+            /*
+            int ancho = 0;
+            int alto = 0;
+
+            Console.WriteLine("Ingrese ancho de la imagen");
+            ancho = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese alto de la imagen ");
+            alto = int.Parse(Console.ReadLine());
+
+            if(ancho > alto)
+            {
+                Console.WriteLine("La imagen es horizontal ");
+
+            }
+            else
+            {
+                Console.WriteLine("La imagen es vertical ");
+            }
+            */
+
+            //// EJERCICIO NUMERO 4 
+
+            /*
+            float limiteVelocidad;
+            int velocidad;
+            int puntosMerito = 0;
+
+            Console.WriteLine("Ingrese el limite de velocidad");
+            limiteVelocidad = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Velocidad del automovil");
+            velocidad = int.Parse(Console.ReadLine());
+
+            if(velocidad <= limiteVelocidad)
+            {
+                Console.WriteLine("OK");
+            }
+            else if(puntosMerito>=12){
+            {
+                Console.WriteLine("Supero el limitede de puntos. LICENCIA SUSPENDIDA");
+            }
+            }
+            */
+        }
+    }
+}
